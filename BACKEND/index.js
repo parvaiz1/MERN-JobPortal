d9845debf2e6e3dd6c6ded1ccd9afc85d6d4a58a
parvaiz1 <97357523+parvaiz1@users.noreply.github.com>
2023-11-03 16:21:54 +0530

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -30,7 +30,7 @@ app.use(cors(
 ))
 app.use(express.static('public'))
 app.use(express.json())
-app.get("/", async(req,res)=>{
+app.get("/", (req,res)=>{
     res.send("Hello")})
 app.use("/StudentProfile",StudentProfileRoutes)
 app.use("/EmpProfile",EmpProfileRoutes)
